da7c537b3c70245ea713a1f4c44149da922275a5
Carolina Guzman <carolinaguzman76@gmail.com>
2020-08-31 18:13:12 +0200

    rutas I

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'layout', pathMatch: 'full' },
+  { path: 'layout', component: LayoutComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '**', redirectTo: 'layout', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
+
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
