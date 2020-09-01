052328573f0f8d8a39b10dea761e754abe96b1b1
Carolina Guzman <carolinaguzman76@gmail.com>
2020-09-01 11:31:14 +0200

    rutas II y componente card

---
+++ src/app/dashboard/list-usuarios/card-usuario/card-usuario.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-usuario',
+  templateUrl: './card-usuario.component.html',
+  styleUrls: ['./card-usuario.component.css']
+})
+export class CardUsuarioComponent implements OnInit {
+  imgUrl = 'http://lorempixel.com/output/people-q-c-640-480-1.jpg';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
